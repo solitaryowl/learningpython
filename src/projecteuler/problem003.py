05047fa961f1584ef8738cd7baad2eaaa1ddee25
solitaryowl <aks.521@gmail.com>
2024-03-14 21:54:37 +0530

    Working on pe003, regex first checkin

---
+++ src/projecteuler/problem003.py
@@ -0,0 +1,10 @@
+"""
+Link:https://projecteuler.net/problem=3
+
+The Prime Factors of 13195 are 5,7,13 and 29
+What is the largest prime factor of the number 600851475143 ?
+
+"""
+
+def  largest_prime_factor(num):
+
---
+++ src/regex/regex101.py
@@ -0,0 +1,33 @@
+"""
+Link:https://runestone.academy/ns/books/published/py4e-int/regex/hp-practice-adv.html
+
+"""
+# Create a regex that finds dates in the format MM/DD/YY or MM/DD/YYYY and returns just the year part
+import re
+
+
+
+def regex1(text):
+    x = re.search("\\d{2}/\\d{2}/\\d{2,4}", text)
+    if x is not None:
+        print(x.group())
+
+
+
+
+# Create a regex that finds phone numbers like 333-232-3403 or (333) 232 3403.
+def regex2(text):
+    x = re.search("\\d{3}-\\d{3}-\\d{4}|\(\\d{3}\\)\s\\d{3}\s\\d{4}", text)
+    if x is not None:
+        print(x.group())
+
+#Create a regex that returns only the first alphabetic word (upper and lower case) at the start of the string
+
+
+
+
+if __name__ == "__main__":
+    regex1(" today is 12/10/2022")
+    regex1(" today is 12/12/22")
+    regex2(" please contact us at 333-223-3403")
+    regex2(" please contact us at (333) 223 3403")
